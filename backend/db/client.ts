302ce5e605e38201ffa9e06079b2e1ebb89b0a5f
r1chwo0n <woonzy09@gmail.com>
2025-07-01 00:42:10 +0700

    fix: switch to session last time

--- backend/db/client.ts
+++ backend/db/client.ts
@@ -3,9 +3,17 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 import { Pool } from "pg";
 // import { connectionString } from "./utils";
-import { pgConfig } from "./utils";
-const pool = new Pool(pgConfig);
-
+// import { pgConfig } from "./utils";
+const pool = new Pool({
+  host: "aws-0-ap-southeast-1.pooler.supabase.com",
+  port: 5432,
+  user: "postgres",
+  password: "Nawa_09062546",
+  database: "postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const dbClient = drizzle(pool, {
   schema,
--- backend/db/migrate.ts
+++ backend/db/migrate.ts
@@ -4,9 +4,17 @@ import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { Pool } from "pg";
 // import { connectionString } from "./utils";
 
-import { pgConfig } from "./utils";
-const pool = new Pool(pgConfig);
-
+// import { pgConfig } from "./utils";
+const pool = new Pool({
+  host: "aws-0-ap-southeast-1.pooler.supabase.com",
+  port: 5432,
+  user: "postgres",
+  password: "Nawa_09062546",
+  database: "postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 async function main() {
   const db = drizzle(pool);
   await migrate(db, {
