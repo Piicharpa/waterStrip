b8170d44d03d02bf2be161186f2404cf37442cbe
r1chwo0n <woonzy09@gmail.com>
2025-07-01 00:05:22 +0700

    fix: switvh back to direct URL

--- backend/db/client.ts
+++ backend/db/client.ts
@@ -5,15 +5,12 @@ import { Pool } from "pg";
 import { connectionString } from "./utils";
 
 const pool = new Pool({
-  host: 'aws-0-ap-southeast-1.pooler.supabase.com',
-  port: 5432,
-  user: 'postgres.bddrbvyrfmswpkljffim',
-  password: 'Nawa_09062546',
-  database: 'postgres',
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
 
 export const dbClient = drizzle(pool, {
   schema,
